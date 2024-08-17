87a7d187e4483f1832681e7578bc38c063c23108
eoinreilly <eoindreilly@gmail.com>
2024-08-17 18:27:12 +0100

    chore: Add TODO for purchasing service

--- src/main/java/com/shop/generic/purchasingservice/services/PurchasingService.java
+++ src/main/java/com/shop/generic/purchasingservice/services/PurchasingService.java
@@ -28,7 +28,7 @@ public class PurchasingService {
     // - Request to product-service to check stock for items
     // - Check payment is valid
     // - Update stock of item in database and make request to order-service to create the order
-    // - Look into how this would be done if this service could not access the products table
+    // - TODO: Look into how this would be done if this service could not access the products table
 
     public RestApiResponse<OrderResponseDTO> purchaseProducts(
             final List<PurchaseProductDTO> purchaseProductDTOS)
